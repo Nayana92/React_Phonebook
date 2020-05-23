6b24fa600ec6cd5c3881a21d3ce3b311ab75d216
Nayana <nlaxmeshwar@mail.sfsu.edu>
2020-05-23 15:49:53 -0700

    Updates

---
+++ precache-manifest.cd1039a90b4fde38a9f7ea399d062ff5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3284c3f002114a4c1b4ea3567e15a138",
+    "url": "/React_Phonebook/index.html"
+  },
+  {
+    "revision": "68c586f8f4abaffbfd56",
+    "url": "/React_Phonebook/static/css/main.f7c42a7c.chunk.css"
+  },
+  {
+    "revision": "eb372fdf8b693f5b753d",
+    "url": "/React_Phonebook/static/js/2.b211e1e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_Phonebook/static/js/2.b211e1e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68c586f8f4abaffbfd56",
+    "url": "/React_Phonebook/static/js/main.b3edfa0c.chunk.js"
+  },
+  {
+    "revision": "22ea92cd75e2c4c37ed8",
+    "url": "/React_Phonebook/static/js/runtime-main.adac0dd9.js"
+  }
+]);
